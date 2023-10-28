c8d7b75ccb0a0fefd5240ff32caf656cef0b34d8
Akarsh Bhagat <abhagat@argusoft.com>
2023-10-28 18:14:46 +0530

    Homepage improved

--- Backend/src/main/java/MedTechBackend/Backend/security/controller/DemoController.java
+++ Backend/src/main/java/MedTechBackend/Backend/security/controller/DemoController.java
@@ -1,6 +1,5 @@
 package MedTechBackend.Backend.security.demo;
 
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,4 +13,5 @@ public class DemoController {
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello from secured endpoint");
     }
+
 }
---
+++ Frontend/src/app/services/login.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  url= "http://localhost:8080/api/v1/auth"
+  constructor(private http: HttpClient) { }
+  // calling server to generate token
+  generateToken(credentials:any){
+    return this.http.post(`${this.url}/authenticate`, credentials)
+  }
+    //for login user
+    loginUser(token: any){
+      localStorage.setItem("token", token)
+      return true;
+    }
+  // to check that user is logeed in
+    isLoggedIn(){
+     let token= localStorage.getItem("token");
+     if(token==undefined || token==='' || token ==null){
+      return false;
+     }
+     else{
+      return true;
+     }
+    }
+    logout(){
+   localStorage.removeItem('token');
+   return true;
+    }
+    // for getting the token
+    getToken(){
+      return localStorage.getItem("token")
+    }
+}
