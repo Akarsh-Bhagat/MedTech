deee1ad2a4dbcbd7242285e355d4632ec8c07e9a
Himanshu <hjaiswal@argusoft.com>
2023-10-30 17:31:30 +0530

    crud frontend

--- Backend/src/main/java/MedTechBackend/Backend/security/auth/AuthenticationController.java
+++ Backend/src/main/java/MedTechBackend/Backend/security/auth/AuthenticationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
