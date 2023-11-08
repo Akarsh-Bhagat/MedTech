d29968bbea9a30c76a3e0d9647971a110bbc98b6
Akarsh Bhagat <abhagat@argusoft.com>
2023-11-08 18:43:56 +0530

    Patient's page design and backend integration completed

--- Frontend/src/app/patient-view/patient-view.component.ts
+++ Frontend/src/app/patient-view/patient-view.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PatientService } from '../services/patient.service';
 
 @Component({
   selector: 'app-patient-view',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./patient-view.component.css']
 })
 export class PatientViewComponent {
+  patient: any
+  showReports: boolean = false;
+  showHideText: string = "Show Reports";
+
+  constructor( private route: ActivatedRoute,private patientService: PatientService){
+  
+    
+  }
+  
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.patientService.getDataById(id).subscribe((data: any) => {
+        this.patient=data;
+        console.log(data);
+      });
+    });
+  }
+  toggleReports(clinicId: number) {
+    const clinic = this.patient.clinics.find((c: any) => c.id === clinicId);
+    if (clinic) {
+      clinic.showReports = !clinic.showReports;
+      this.showHideText = clinic.showReports ? "Hide Reports" : "Show Reports";
+    }
+  }
 
 }
